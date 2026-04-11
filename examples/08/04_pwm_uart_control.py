7fceac479622f4982b5c97e26178daab8f525c05
AlexMalachevsky <t89257409519@gmail.com>
2026-04-11 08:08:17 +0300

    Add PWM UART

---
+++ examples/08/04_pwm_uart_control.py
@@ -0,0 +1,22 @@
+# Пример 4. Управление PWM через UART
+
+from machine import PWM, FPIOA
+from ybUtils.YbUart import YbUart
+import time
+
+fpioa = FPIOA()
+fpioa.set_function(42, fpioa.PWM0)
+
+pwm = PWM(0, 1000, 50, enable=True)
+uart = YbUart(baudrate=115200)
+
+while True:
+    data = uart.read()
+    if data:
+        try:
+            duty = int(data.decode().strip())
+            pwm.duty(duty)
+            print("Duty set:", duty)
+        except:
+            print("Error")
+    time.sleep_ms(50)
