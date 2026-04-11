3deb7fdeb877e83921553ee93f25da4a1a948c23
AlexMalachevsky <t89257409519@gmail.com>
2026-04-11 08:08:40 +0300

    Add PWM music

---
+++ examples/08/05_pwm_music.py
@@ -0,0 +1,16 @@
+# Пример 5. PWM как музыка (простая)
+
+from machine import PWM, FPIOA
+import time
+
+fpioa = FPIOA()
+fpioa.set_function(42, fpioa.PWM0)
+
+pwm = PWM(0, 1000, 50, enable=True)
+
+notes = [500, 800, 1000, 1200]
+
+while True:
+    for note in notes:
+        pwm.freq(note)
+        time.sleep(0.3)
