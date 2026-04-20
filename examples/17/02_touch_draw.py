5c5f5f4c229e58eeb34b916933993300163de1e6
AlexMalachevsky <t89257409519@gmail.com>
2026-04-20 23:43:12 +0300

    Add touch draw example

---
+++ examples/17/02_touch_draw.py
@@ -0,0 +1,26 @@
+# ============================================
+# K230 Example
+# Рисование пальцем по экрану
+# ============================================
+
+from media.display import *
+from media.media import *
+from media.touch import *
+import image, time
+
+Display.init(Display.ST7701, width=640, height=480, osd_num=1, to_ide=True)
+MediaManager.init()
+Touch.init()
+
+img = image.Image(640, 480, image.RGB565)
+img.clear()
+
+while True:
+    point = Touch.read()
+
+    if point:
+        x, y = point[0], point[1]
+        img.draw_circle(x, y, 3, color=(255, 255, 0), thickness=2)
+
+    Display.show_image(img)
+    time.sleep(0.02)
