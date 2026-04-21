a246231f7ae4ce3035d27ffb48d0dcd4d94ddbd1
AlexMalachevsky <t89257409519@gmail.com>
2026-04-21 06:38:37 +0300

    Update examples/19 basic camera preview for image flip module

--- examples/19/01_camera_basic_channel.py
+++ examples/19/01_camera_basic_channel.py
@@ -1,21 +1,48 @@
-import time, os
+# ============================================
+# K230 Example
+# Camera image flip - normal preview
+# ============================================
+
+import uos as os
+import time
 from media.sensor import *
 from media.display import *
 from media.media import *
 
-sensor = Sensor()
-sensor.reset()
 
-# channel 1 config (recommended)
-sensor.set_framesize(width=640, height=480, chn=CAM_CHN_ID_1)
-sensor.set_pixformat(Sensor.RGB565, chn=CAM_CHN_ID_1)
+def init_sensor():
+    sensor = Sensor()
+    sensor.reset()
+    sensor.set_framesize(width=640, height=480, chn=CAM_CHN_ID_1)
+    sensor.set_pixformat(Sensor.RGB565, chn=CAM_CHN_ID_1)
+    return sensor
 
-Display.init(Display.ST7701, width=640, height=480, to_ide=True)
-MediaManager.init()
 
-sensor.run()
+def main():
+    sensor = None
+    try:
+        sensor = init_sensor()
+        Display.init(Display.ST7701, width=640, height=480, to_ide=True)
+        MediaManager.init()
+        sensor.run()
 
-while True:
-    os.exitpoint()
-    img = sensor.snapshot(chn=CAM_CHN_ID_1)
-    Display.show_image(img)
+        while True:
+            os.exitpoint()
+            img = sensor.snapshot(chn=CAM_CHN_ID_1)
+            Display.show_image(img)
+
+    except KeyboardInterrupt:
+        print("User interrupted the program")
+    except Exception as e:
+        print("An error occurred: {}".format(e))
+    finally:
+        if isinstance(sensor, Sensor):
+            sensor.stop()
+        Display.deinit()
+        os.exitpoint(os.EXITPOINT_ENABLE_SLEEP)
+        time.sleep_ms(100)
+        MediaManager.deinit()
+
+
+if __name__ == "__main__":
+    main()
