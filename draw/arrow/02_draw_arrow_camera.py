e86eb4608d0b5ed5e8977b6f7c593ceb135dfaf6
AlexMalachevsky <t89257409519@gmail.com>
2026-04-25 08:17:06 +0300

    Add arrow camera example

---
+++ draw/arrow/02_draw_arrow_camera.py
@@ -0,0 +1,33 @@
+# ============================================
+# K230 Draw Arrow + Camera
+# ============================================
+
+import uos as os
+from media.sensor import *
+from media.display import *
+from media.media import *
+
+WIDTH = 640
+HEIGHT = 480
+
+sensor = Sensor()
+sensor.reset()
+sensor.set_framesize(width=WIDTH, height=HEIGHT, chn=CAM_CHN_ID_1)
+sensor.set_pixformat(Sensor.RGB565, chn=CAM_CHN_ID_1)
+
+Display.init(Display.ST7701, width=WIDTH, height=HEIGHT, to_ide=True)
+MediaManager.init()
+
+sensor.run()
+
+while True:
+    os.exitpoint()
+    img = sensor.snapshot(chn=CAM_CHN_ID_1)
+
+    # Draw arrows overlay
+    img.draw_arrow(100,100,300,200,color=(255,0,0), thickness=2)
+    img.draw_arrow(300,200,500,300,color=(0,255,0), thickness=2)
+
+    img.draw_string(10,10,"Draw Arrow", color=(255,255,0))
+
+    Display.show_image(img)
