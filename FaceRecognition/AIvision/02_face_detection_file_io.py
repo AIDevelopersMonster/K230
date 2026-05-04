9f100d76900268c80f523530b3082da8aa4e1a6a
AlexMalachevsky <t89257409519@gmail.com>
2026-05-04 06:38:43 +0300

    Update 02_face_detection_file_io.py

--- FaceRecognition/AIvision/02_face_detection_file_io.py
+++ FaceRecognition/AIvision/02_face_detection_file_io.py
@@ -18,7 +18,7 @@
 from libs.PipeLine import PipeLine, ScopedTiming
 import time
 import gc
-
+from libs.face_aivision_common import create_face_detection_app, safe_deinit
 from face_aivision_common import (
     create_face_detection_app,
     safe_deinit,
